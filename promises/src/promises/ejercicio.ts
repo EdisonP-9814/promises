253165ce983ad1199513d26bbfdaf604ebde4923
Jhon P <edip9814@gmail.com>
2024-09-20 23:01:25 -0500

    Implematacion del ternario

--- promises/src/promises/ejercicio.ts
+++ promises/src/promises/ejercicio.ts
@@ -2,6 +2,7 @@ function suma(a:number, b:number){
     new Promise((resolve, renject)=>{
         if(a>0 && b>0){
             resolve('Promesa Cumplida')
+            console.log("Solucion de la Suma = ",a+b)
         }else{
             renject('promesa no cumplida')
         }
@@ -10,5 +11,19 @@ function suma(a:number, b:number){
     .finally(()=> console.log('fin de la promesa'))
 }
 
+function division(dividendo:number, divisor:number){
+    new Promise((resolve, renject)=>{
+        if(divisor!=0){
+            resolve("Promesa Cumplida => ")
+            console.log("Solucion de la division")
+        }else{
+            renject("no se puede cumplir la promesa")
+            console.error("No se puede resolver la DIVISION = "+dividendo/divisor)
+        }
+    }).then((mensaje)=> console.log(mensaje))
+    .catch((mensajeError)=> console.error(mensajeError))
+    .finally(()=> console.log('fin de la promesa'))
+}
 
-console.log(suma(10,20));
+suma(10,20);
+division(1,0)
